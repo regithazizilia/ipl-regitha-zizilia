27e10b256d5f622909dd2702c6d9eddd975b6e2c
DESKTOP-H3B7AE3\ThinkBook <regithazizilia@gmail.com>
2023-11-09 22:29:24 +0700

    single-catch-block

--- DefensiveProgramming/ExcepTest.java
+++ DefensiveProgramming/ExcepTest.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class ExcepTest { 
     public static void main(String args[]) { 
+<<<<<<< HEAD
         try { 
             int a[] = new int[2]; 
             System.out.println("Access element three :" + a[3]); 
@@ -12,3 +13,14 @@ public class ExcepTest {
     } 
 } 
 
+=======
+    try { 
+        int a[] = new int[2]; 
+        System.out.println("Access element three :" + a[3]); 
+    } catch (ArrayIndexOutOfBoundsException e) { 
+        System.out.println("Exception thrown :" + e); 
+    } 
+    System.out.println("Out of the block"); 
+    } 
+} 
+>>>>>>> Single-Catch-Block
