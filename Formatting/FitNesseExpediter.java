82ce2b8c6a87afa39d4e7f251bb157605e58a667
DESKTOP-H3B7AE3\ThinkBook <regithazizilia@gmail.com>
2023-11-09 19:21:05 +0700

    Penambahan code pada Formatting

--- Formatting/FitNesseExpediter.java
+++ Formatting/FitNesseExpediter.java
@@ -2,15 +2,16 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class FitNesseExpediter implements ResponseSender {
-    private Socket socket;
-    private InputStream input;
-    private OutputStream output;
-    private Request request;
-    private Response response;
-    private FitnesseContext context;
-    protected long requestParsingTimeLimit;
-    private long requestProgres;
-    private long requestParsingDeadLine;
-    private Boolean hasError;
-} 
+public class FitNesseExpediter implements ResponseSender { 
+    private     Socket          socket; 
+    private     InputStream     input; 
+    private     OutputStream    output; 
+    private     Request         request; 
+    private     Response        response; 
+    private     FitNesseContext context; 
+    protected   long            requestParsingTimeLimit; 
+    private     long            requestProgress; 
+    private     long            requestParsingDeadline; 
+    private     boolean         hasError; 
+
+}
