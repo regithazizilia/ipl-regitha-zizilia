ad3ea4453c866d45427fee9e26c206ee60809c09
DESKTOP-H3B7AE3\ThinkBook <regithazizilia@gmail.com>
2023-11-09 22:42:25 +0700

    Penambahan kelas User.java

---
+++ cleancode/User.java
@@ -0,0 +1,9 @@
+class User { 
+    protected String n; //username 
+    protected String p; //password 
+ 
+    public void penyimpanan(String n, String p) { 
+        this.username = username;
+        this.password = password;
+    } 
+}
